1e7704da250ea0fdd9b4ee18a616fbcb4d5ad31a
Kauã Pereira <zkauacodes@hotmail.com>
2023-03-04 20:30:38 -0300

    refactor(home): add keycloak integration

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,48 +1,31 @@
 import { useKeycloak } from "@react-keycloak/web";
-import { useEffect, useState } from "react";
-import Api from "./utils/api";
-
-type IUser = {
-  userId: string | undefined;
-  profileId: string | undefined;
-  email: string | undefined;
-  avatarUrl: string | undefined;
-};
+import { useEffect } from "react";
+import { useAuth } from "./context/auth/useAuth";
 
 const Home = () => {
   const { keycloak, initialized } = useKeycloak();
-  const [user, setUser] = useState<IUser>();
-  // verificar se já existe no state se não seta de novo
-  // ou então criar um método que busca / cria o user e chamar ele quando precisar,
-  // se existir no localstorage pega de lá se não vai ao backend
-  useEffect(() => {
-    async function recover() {
-      if (initialized && keycloak.authenticated) {
-        const response = await Api.get(`/profile/${keycloak.tokenParsed?.sub}`)
-          .then((response) => {
-            if (response.status === 200) {
-              console.log(response.data);
-            }
-          })
-          .catch((err) => {
-            if (err.response.status === 404) {
-              Api.post("/profile", {
-                userId: keycloak.tokenParsed?.sub,
-                avatarUrl: null,
-              });
-            }
-          });
+  const { user, retrieveOrCreate } = useAuth();
 
-        return response;
+  useEffect(() => {
+    async function data() {
+      if (initialized && keycloak.authenticated) {
+        await retrieveOrCreate({
+          userId: keycloak.tokenParsed?.sub,
+          username: keycloak.tokenParsed?.preferred_username,
+          email: keycloak.tokenParsed?.email,
+          avatarUrl: keycloak.tokenParsed?.avatarUrl,
+        });
       }
     }
-    recover();
+
+    data();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [initialized, keycloak]);
 
   return (
     <div>
       <h1 className="text-green-800 text-4xl">
-        Welcome {user?.email} to the Homepage
+        Welcome {user?.userId} to the Homepage
       </h1>
     </div>
   );
