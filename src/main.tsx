69aa75cb47f2a0bb411eb1fcb1021dd8e9c9c9a6
Kauã Pereira <zkauacodes@hotmail.com>
2023-03-04 20:31:09 -0300

    feat(main): add the auth provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { ReactKeycloakProvider } from "@react-keycloak/web";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import AuthProvider from "./context/auth/AuthProvider";
 import "./index.css";
 import keycloak, { keycloakProviderInitConfig } from "./utils/auth";
 
@@ -10,6 +11,8 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     authClient={keycloak}
     initOptions={keycloakProviderInitConfig}
   >
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </ReactKeycloakProvider>
 );
