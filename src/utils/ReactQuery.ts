dbc9ac7dc6778fac04d570daeb767d8e295b261f
Kauã Pereira <zkauacodes@hotmail.com>
2023-04-18 19:03:44 -0300

    feat(configs): add log configs and configure env variables

--- src/utils/ReactQuery.ts
+++ src/utils/ReactQuery.ts
@@ -1,3 +1,9 @@
 import { QueryClient } from "@tanstack/react-query";
 
-export const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+  logger: {
+    log: console.log,
+    warn: console.warn,
+    error: import.meta.env.VITE_NODE_ENV === "prod" ? () => {} : console.error,
+  },
+});
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: import.meta.env.VITE_BACKEND_URL,
   headers: { "Access-Control-Allow-Origin": "*" },
 });
 
