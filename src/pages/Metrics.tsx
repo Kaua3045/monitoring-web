75fc83513cb8166b5d20c530f55df92d9acd92a8
Kauã Pereira <zkauacodes@hotmail.com>
2023-04-28 13:42:30 -0300

    feat(metrics): add the responsive elements

--- src/pages/Metrics.tsx
+++ src/pages/Metrics.tsx
@@ -90,8 +90,8 @@ const Metrics = () => {
       <Nav />
 
       <div className="flex items-center mt-28 flex-col">
-        <div className="bg-slateDark-650 w-2/4 h-auto flex flex-col items-start pl-2 gap-3 rounded-t-[3px] rounded-tr-[3px]">
-          <fieldset className="flex items-center gap-2 mt-2">
+        <div className="bg-slateDark-650 w-2/4 max-sm:w-auto h-auto flex flex-col items-start max-sm:items-center max-sm:pr-3 max-sm:pl-3 pl-2 gap-3 rounded-t-[3px] rounded-tr-[3px]">
+          <fieldset className="flex max-sm:flex-col items-center max-sm:gap-1 gap-2 mt-2">
             <label htmlFor="start-timestamp" className="text-white-100">
               Inicio
             </label>
@@ -103,7 +103,7 @@ const Metrics = () => {
             />
           </fieldset>
 
-          <fieldset className="flex items-center gap-[21px] mb-2">
+          <fieldset className="flex items-center max-sm:flex-col max-sm:gap-1 gap-[21px] mb-2">
             <label htmlFor="fim-timestamp" className="text-white-100">
               Fim
             </label>
@@ -126,11 +126,11 @@ const Metrics = () => {
           </div>
         </div>
 
-        <div className="bg-slateDark-650/90 w-2/4 h-auto shadow-md rounded-bl-md rounded-br-md">
-          <div className="flex justify-around items-center">
+        <div className="bg-slateDark-650/90 w-2/4 max-sm:w-[232px] h-auto shadow-md rounded-bl-md rounded-br-md">
+          <div className="flex justify-around items-center max-sm:flex-col max-sm:justify-center">
             <h3 className="text-white-100/80">{url.title}</h3>
             {startTime.length <= 0 || endTime.length <= 0 ? (
-              <p className="text-white-100/80">últimos 30 dias</p>
+              <p className="text-white-100/80">Últimos 30 dias</p>
             ) : (
               <p className="text-white-100/80">{formatDate()}</p>
             )}
