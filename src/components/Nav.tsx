58b19392c7afbfbf0b2b5eb503d0255357b66887
Kauã Pereira <zkauacodes@hotmail.com>
2023-04-28 12:11:50 -0300

    fix(nav): fix buttons in mobile view

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -6,7 +6,7 @@ const Nav = () => {
 
   return (
     <header aria-label="Site Header" className="bg-slateDark-50 shadow-lg">
-      <div className="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8">
+      <div className="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-2 lg:px-8">
         <a className="block text-white-100 hover:text-blue-1003" href="/">
           <span className="sr-only">Home</span>
           <h1 className="text-2xl">Monitore</h1>
@@ -39,9 +39,9 @@ const Nav = () => {
               {token ? (
                 <Profile />
               ) : (
-                <>
+                <div className="flex items-center gap-4">
                   <a
-                    className="block rounded-md bg-blue-400 px-5 py-2.5 text-sm font-medium text-blue-1002 transition hover:opacity-50"
+                    className="rounded-md bg-blue-400 px-5 py-2.5 text-sm font-medium text-blue-1002 transition hover:opacity-50"
                     href="/login"
                   >
                     Login
@@ -52,7 +52,7 @@ const Nav = () => {
                   >
                     Register
                   </a>
-                </>
+                </div>
               )}
             </div>
           </div>
